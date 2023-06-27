594b3fd52a80e028168d4341e16bc858dd11cb47
Manoj Patil <manoj.patil@baptisthealth.net>
2023-06-27 16:58:15 +0530

    Execure Provisioning Pipeline

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,4 @@
+################### This comment added to test pipe line ###############
 resource "aws_iam_role" "aft_states" {
   name               = "aft-account-provisioning-customizations-role"
   assume_role_policy = templatefile("${path.module}/iam/trust-policies/states.tpl", { none = "none" })
